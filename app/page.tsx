5dbdd6292d6bad8dbcaada8eebd1c25720928fc9
gilang harman <alfarizkizhafirlawfirm@gmail.com>
2026-09-22 20:16:20 +0700

    Raise WA button to 160px desktop 150px mobile to clear Netlify badge completely

--- app/page.tsx
+++ app/page.tsx
@@ -3405,7 +3405,7 @@ export default async function Home() {
           style={{
             position: "fixed",
             right: "24px",
-            bottom: "120px",
+            bottom: "160px",
             zIndex: "60",
             display: "flex",
             alignItems: "center",
